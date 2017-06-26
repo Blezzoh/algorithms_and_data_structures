686d495e91bfcc3e81fa31b5ba19b0ae31635a6d
blezzoh <iradub0@sewanee.edu>
2017-06-26 12:37:09 -0500

    more recursion problems

--- src/algorithms/RecursionProblems.java
+++ src/algorithms/RecursionProblems.java
@@ -1,7 +1,9 @@
 package algorithms;
 
+
 /**
  * Created by blaise on 6/26/17.
+ * Exercise about recursion and answers
  */
 public class RecursionProblems {
 
@@ -34,7 +36,7 @@ Given a non-negative int n, return the sum of its digits recursively (no loops).
 Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
 while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
     */
-    public int sumDigits(int n) {
+    public static int sumDigits(int n) {
         if (n <0){
             System.out.println("accepts only positive inputs");
             return -1;
@@ -53,7 +55,7 @@ while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
     Given a non-negative int n, return the count of the occurrences
     of 7 as a digit, so for example 717 yields 2. (no loops).
      */
-    public int count7(int n) {
+    public static int count7(int n) {
         if(n<10){
             if(n == 7)
                 return 1;
@@ -69,30 +71,138 @@ while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
             else return 0 + count7(n);
         }
     }
+    /*
+
+    Given a string, compute recursively (no loops)
+    the number of lowercase 'x' chars in the string.
+    */
+    public static int countX(String str) {
+        int n = str.length() - 1;
+        return countX(str, n);
+    }
+
+    public static int countX(String str, int n){
+        if(n == -1)
+            return 0;
+        else if(n == 0){
+            if(str.charAt(0) == 'x')
+                return 1;
+            else
+                return 0;
+        }
+        else{
+            if(str.charAt(n) == 'x')
+                return 1 + countX(str, n-1);
+            else
+                return 0 + countX(str, n-1);
+        }
+    }
+
+    /*
+    Given a string, compute recursively (no loops) the number of times lowercase "hi"
+    appears in the string.
+     */
+    public static int countHi(String str) {
+        int n = str.length() - 1;
+        return countHi(str, n);
+    }
+
+
+    public static int countHi(String str,int n){
+        if(n-1 < 0){
+            return 0;
+        }
+        else if(n == 1){
+
+            if(str.substring(0, 2).equals("hi"))
+                return 1;
+
+            else
+                return 0;
+        }
+        else{
+
+            if(str.substring(n-1, n+1).equals("hi"))
+                return 1 + countHi(str, n-1);
+
+            else
+                return countHi(str, n-1);
+        }
+    }
+
+    /*
+    Given a string, compute recursively (no loops) a new string where
+    all the lowercase 'x' chars have been changed to 'y' chars.
+     */
+    public static String changeXY(String str) {
+        char c [] = str.toCharArray();
+        return changeXY(c, c.length-1);
+    }
+
+    public static String changeXY(char [] c ,int n){
+        if(n < 0){
+            return "";
+        }
+
+        if (n == 0){
+
+            if(c [0] == 'x') c[0] = (char) 'y';
+            return String.valueOf(c);
+
+
+        }
+        else{
+            if(c [n] == 'x') c[n] = (char) 'y';
+            return changeXY( c , n-1);
+
+        }
+    }
+
     public static void main(String [] args){
         RecursionProblems recur = new RecursionProblems();
         // bunny ears problems
-        System.out.println("bunnyEars(0): " + recur.bunnyEars(0));
-        System.out.println("bunnyEars(1): " + recur.bunnyEars(1));
-        System.out.println("bunnyEars(2): " + recur.bunnyEars(2));
-        System.out.println("bunnyEars(3): " + recur.bunnyEars(3));
-        System.out.println("bunnyEars(10): " + recur.bunnyEars(10));
-        System.out.println("bunnyEars(-5): " + recur.bunnyEars(-5));
+        System.out.println("bunnyEars(0): " + bunnyEars(0));
+        System.out.println("bunnyEars(1): " + bunnyEars(1));
+        System.out.println("bunnyEars(2): " + bunnyEars(2));
+        System.out.println("bunnyEars(3): " + bunnyEars(3));
+        System.out.println("bunnyEars(10): " + bunnyEars(10));
+        System.out.println("bunnyEars(-5): " + bunnyEars(-5) + "\n");
+
         //sumDigits
-        System.out.println("sumDigits(0): " + recur.sumDigits(0));
-        System.out.println("sumDigits(5): " + recur.sumDigits(5));
-        System.out.println("sumDigits(115): " + recur.sumDigits(115));
-        System.out.println("sumDigits(123456789): " + recur.sumDigits(123456789));
-        //count occourence of 7
-        System.out.println("count(0): " + recur.count7(0));
-        System.out.println("count(127): " + recur.count7(127));
-        System.out.println("count(7): " + recur.count7(7));
-        System.out.println("count(1274357778): " + recur.count7(1274357778));
-        System.out.println("count(1123456890): " + recur.count7(1123456890));
+        System.out.println("sumDigits(0): " + sumDigits(0));
+        System.out.println("sumDigits(5): " + sumDigits(5));
+        System.out.println("sumDigits(115): " + sumDigits(115));
+        System.out.println("sumDigits(123456789): " + sumDigits(123456789)+ "\n");
 
+        //count occourrence of 7
+        System.out.println("count(0): " + count7(0));
+        System.out.println("count(127): " + count7(127));
+        System.out.println("count(7): " + count7(7));
+        System.out.println("count(1274357778): " + count7(1274357778));
+        System.out.println("count(1123456890): " + count7(1123456890)+ "\n");
 
+        //count occurrence of x
+        System.out.println("countX(\"xxhixx\"): " + countX("xxhixx") );
+        System.out.println("countX(\"hi\"): " + countX("hi") );
+        System.out.println("countX(\"h\"): " + countX("h") );
+        System.out.println("countX(\"x\"): " + countX("x") );
+        System.out.println("countX(\"\"): " + countX("")+ "\n");
 
+        //count occurrence of hi
+        System.out.println("countHi(\"xxhixx\"): " + countHi("xxhixx") );
+        System.out.println("countHi(\"\"): " + countHi(""));
+        System.out.println("countHi(\"ihihihihih\") : " + countHi("ihihihihih") );
+        System.out.println("countHi(\"hiAAhi12hi\"): " +countHi("hiAAhi12hi") );
+        System.out.println("countHi(\"xhixhxihihhhih\"): " + countHi("xhixhxihihhhih"));
+        System.out.println("countHi(\"ship\"): " + countHi("ship")+ "\n");
 
+        // change x to y
+        System.out.println("changeXY(\"codey\"): " + changeXY("codey"));
+        System.out.println("changeXY(\"\") : " + changeXY("")  );
+        System.out.println("changeXY(\"xxx\") : " + changeXY("xxx") );
+        System.out.println("changeXY(\"yyhxyi\"): " + changeXY("yyhxyi"));
+        System.out.println("changeXY(\"hihi\"): " + changeXY("hihi"));
+        System.out.println("changeXY(\"xhixhix\"): " + changeXY("xhixhix")+ "\n");
 
     }
 
