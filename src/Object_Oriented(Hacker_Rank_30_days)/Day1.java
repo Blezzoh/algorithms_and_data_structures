4a2d0f51595a00d02bdee0fc7f534703484455ab
Blaise Iradukunda <iradub0@sewanee.edu>
2017-08-08 17:48:21 -0500

    Update Day1.java

--- src/Object_Oriented(Hacker_Rank_30_days)/Day1.java
+++ src/Object_Oriented(Hacker_Rank_30_days)/Day1.java
@@ -1,21 +1,20 @@
-        /* Declare second integer, double, and String variables. */
-        int a;
-        double b;
-        String c;
 
-        /* Read and save an integer, double, and String to your variables.*/
-        a = scan.nextInt();
-        b = scan.nextDouble();
-        scan.nextLine();
-        c = scan.nextLine();
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-        
-        /* Print the sum of both integer variables on a new line. */
-        System.out.println(a+i);
-
-        /* Print the sum of the double variables on a new line. */
-        System.out.println(b + d);
+public class Day1 {
+	public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in); 
 		
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
-        System.out.println(s + c);
+		// Read a full line of input from stdin and save it to our variable, inputString.
+		String inputString = scan.nextLine(); 
+
+		// Close the scanner object, because we've finished reading 
+        // all of the input from stdin needed for this challenge.
+		scan.close(); 
+      
+		// Print a string literal saying "Hello, World." to stdout.
+		System.out.println("Hello, World.");
+      
+	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
+        System.out.println(inputString);
+	}
+}
