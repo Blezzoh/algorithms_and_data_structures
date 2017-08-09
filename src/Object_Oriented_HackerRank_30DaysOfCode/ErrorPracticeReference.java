7584bc04e3fc1187d0c7fcd643b84694b8d9c754
Blaise Iradukunda <iradub0@sewanee.edu>
2017-08-09 12:51:36 -0500

    Update ErrorPracticeReference.java

--- src/Object_Oriented_HackerRank_30DaysOfCode/ErrorPracticeReference.java
+++ src/Object_Oriented_HackerRank_30DaysOfCode/ErrorPracticeReference.java
@@ -1,7 +1,10 @@
 class ErrorPracticeReference {
 
     /**
+     * the "throws" statement is used on the method's declaration for an exception 
+     that the method might possibly throw
      * throws statement followed by the type of error the method will throw
+     * the "throw" statememt is used to throw a "a user-defined exception"
      * throw statement inside the method followed by creation of a new Exception already 
      mentioned(by the throws statement) in the method header
      */
@@ -9,6 +12,10 @@ class ErrorPracticeReference {
         throw new Exception("This exception will always be thrown.");
     }
     
+    /**
+     * try and catch are on a block statement that you already know might throw an error
+     * you know this because the method header has a "throws"
+     */
     public static void main(String[] args) {
         PropagatedException p = new PropagatedException();
         try{
