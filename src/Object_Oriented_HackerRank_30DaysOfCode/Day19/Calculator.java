6bf246708138425f7e6283b48e3860126fbc0da9
Blaise Iradukunda <iradub0@sewanee.edu>
2017-08-10 16:47:16 -0500

    Create Calculator.java

---
+++ src/Object_Oriented_HackerRank_30DaysOfCode/Day19/Calculator.java
@@ -0,0 +1,13 @@
+//Write your code here
+oublic class Calculator implements AdvancedArithmetic{
+    public int divisorSum(int n){
+        int sum = 1;
+        for (int i=2; i<=n; i++){
+            if(n%i == 0)
+                sum += i;
+        }
+        
+        return sum;
+
+    }
+}
