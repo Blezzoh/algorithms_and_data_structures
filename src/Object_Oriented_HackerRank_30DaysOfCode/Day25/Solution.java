9e6b087940313c0ed373df5a3250cae4dc7ea5a3
blezzoh <iradub0@sewanee.edu>
2017-08-30 16:46:01 -0500

    day 25

---
+++ src/Object_Oriented_HackerRank_30DaysOfCode/Day25/Solution.java
@@ -0,0 +1,34 @@
+package Object_Oriented_HackerRank_30DaysOfCode.Day25;
+
+/**
+ * Created by blaise on 8/30/17.
+ */
+import java.util.*;
+
+public class Solution {
+    public static boolean isPrime(int n){
+        if(n%2 == 0){
+            return false;
+        }
+        for(int i = 3; i< Math.ceil(n/2.0); i +=2){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n>0){
+            if(isPrime(sc.nextInt())){
+                System.out.println("Prime");
+            }
+            else{
+                System.out.println("Not prime");
+            }
+            n --;
+        }
+    }
+}
