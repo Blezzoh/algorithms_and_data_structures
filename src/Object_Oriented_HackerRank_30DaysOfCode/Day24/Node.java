03f1bd20a95e2db5a7814fb2df8931f78272c464
blezzoh <iradub0@sewanee.edu>
2017-08-30 16:26:31 -0500

    reformatted code

--- src/Object_Oriented_HackerRank_30DaysOfCode/Day24/Node.java
+++ src/Object_Oriented_HackerRank_30DaysOfCode/Day24/Node.java
@@ -1,8 +1,11 @@
-class Node{
-  int data;
-  Node next;
-  Node(int d){
-   data=d;
-   next=null;
-   }
+package Object_Oriented_HackerRank_30DaysOfCode.Day24;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
 }
