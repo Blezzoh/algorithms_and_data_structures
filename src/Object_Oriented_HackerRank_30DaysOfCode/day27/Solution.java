571aa36dd9e8dd068da9a6182a2ecc633c9f52f0
Blaise Iradukunda <iradub0@sewanee.edu>
2017-10-31 23:42:11 -0500

    Create Solution.java

---
+++ src/Object_Oriented_HackerRank_30DaysOfCode/day27/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void sout(String str){
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        /* test*/
+        sout("5");
+        
+        sout("5 4");
+        sout("-1 0 3 3 1");
+        
+        sout("6 3");
+        sout("-1 10 1 2 3 2");
+        
+        sout("8 4");
+        sout("-1 -2 0 3 3 9 6 1");
+        
+        sout("3 3");
+        sout("-2 0 1");
+        
+        sout("8 5");
+        sout("-4 -1 0 2 3 5 6 0");
+        
+    }
+}
